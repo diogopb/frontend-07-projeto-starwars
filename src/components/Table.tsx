c53be184bc60d66546e9b5464d41986fc70d8815
Diogo Pinto <dpintobarbosa@gmail.com>
2024-02-21 12:38:05 -0300

    Requisito 3 incompleto

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,7 +2,17 @@ import React, { useContext } from 'react';
 import context from '../context/Context';
 
 function Table() {
-  const { data, setSearch, searched } = useContext(context);
+  const {
+    data,
+    setSearch,
+    searched,
+    columns,
+    selectedColumn,
+    comparisons,
+    comparisonsValue,
+    handleComparisons,
+    handleOptions } = useContext(context);
+  const options = ['maior que', 'menor que', 'igual a'];
 
   if (data.length === 0) {
     return <div>error</div>;
@@ -16,6 +26,47 @@ function Table() {
 
   return (
     <div>
+      <div>
+        <label htmlFor="column">Coluna</label>
+        <select
+          name="column"
+          data-testid="column-filter"
+          value={ selectedColumn }
+          onChange={ ({ target }) => handleOptions(target) }
+        >
+          {columns.map((column: any) => (
+            <option key={ column }>{column}</option>
+          ))}
+        </select>
+
+        <label htmlFor="comparison">Operator</label>
+        <select
+          name="comparison"
+          data-testid="comparison-filter"
+          value={ comparisons }
+          onChange={ (e) => handleOptions(e) }
+        >
+          {options.map((option) => (
+            <option key={ option }>{option}</option>
+          ))}
+        </select>
+
+        <input
+          type="number"
+          name="value"
+          data-testid="value-filter"
+          value={ comparisonsValue }
+          onChange={ (e) => handleOptions(e) }
+        />
+
+        <button
+          data-testid="button-filter"
+          onClick={ handleComparisons }
+        >
+          Filter
+        </button>
+      </div>
+
       <input
         type="text"
         data-testid="name-filter"
--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -3,9 +3,19 @@ import Context from './Context';
 import { PlanetType } from '../utils/types';
 
 function Provider({ children }: { children: React.ReactNode }) {
-  const [dataApi, setDataApi] = useState<PlanetType[]>([]);
+  const [data, setData] = useState<PlanetType[]>([]);
   const [search, setSearch] = useState('');
-  const [searched, setSearched] = useState<PlanetType[]>([]);
+  const [searched, setSearched] = useState<PlanetType[]>(data);
+  const [columns, setColumns] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+  const [selectedColumn, setSelectedColumn] = useState(columns[0]);
+  const [comparisons, setComparisons] = useState('maior que');
+  const [comparisonsValue, setComparisonsValue] = useState(0);
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -16,19 +26,81 @@ function Provider({ children }: { children: React.ReactNode }) {
         const { residents, ...notResidents } = planet;
         return notResidents;
       });
-      setDataApi(planets);
-    };
-    const planetSearched = () => {
-      const newData = dataApi.filter((planet) => planet.name
-        .includes(search.toLowerCase()));
-      setSearched(newData);
+      console.log('Dados buscados', planets);
+      setData(planets);
+      setSearched(planets);
     };
     fetchApi();
+  }, []);
+
+  useEffect(() => {
+    const planetSearched = () => {
+      const newData = data.filter((planet) => planet.name
+        .toLowerCase().includes(search.toLowerCase()));
+      setSearched(newData);
+    };
     planetSearched();
-  }, [dataApi, search]);
+  }, [search, data]);
+
+  const handleColumns = () => {
+    if (columns.length !== 6) {
+      setColumns(columns.filter((column) => column !== selectedColumn));
+    }
+    setSelectedColumn(columns[0]);
+  };
+
+  const handleComparisons = () => {
+    console.log('handleComparisons chamado');
+
+    const filteredColumns = searched.filter((planet: any) => {
+      const value = Number(planet[selectedColumn]);
+      const comparison = Number(comparisonsValue);
+
+      if (comparisons === 'maior que') {
+        return value > comparison;
+      } if (comparisons === 'menor que') {
+        return value < comparison;
+      }
+      return value === comparison;
+    });
+    console.log('filteredColumns', filteredColumns);
+    setSearched(filteredColumns);
+    handleColumns();
+  };
+
+  const handleOptions = (target: any) => {
+    console.log(target);
+    if (target.name === 'column') {
+      setSelectedColumn(target.value);
+    } else if (target.name === 'comparison') {
+      setComparisons(target.value);
+    } else {
+      setComparisonsValue(target.value);
+    }
+  };
+
+  const contextValue = {
+    data,
+    setData,
+    search,
+    setSearch,
+    searched,
+    setSearched,
+    columns,
+    setColumns,
+    selectedColumn,
+    setSelectedColumn,
+    comparisons,
+    setComparisons,
+    comparisonsValue,
+    setComparisonsValue,
+    handleColumns,
+    handleComparisons,
+    handleOptions,
+  };
 
   return (
-    <Context.Provider value={ { data: dataApi, setSearch, searched } }>
+    <Context.Provider value={ contextValue }>
       {children}
     </Context.Provider>
   );
